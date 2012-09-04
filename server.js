89d8d7c594e2218b157d78fcb3a4676115985fc7
Likun Liu <likun.liu@rea-group.com.au>
2012-09-04 14:42:59 +1000

    fixed mongo for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var fs = require('fs'),
         https = require('https'),
         url = require('url'),
         mongo = require('mongoskin'),
-        db = mongo.db('127.0.0.1:27017/mydb?auto_reconnect=true'),
+        db = mongo.db('mongodb://heroku:b7828f91799ed3e4a3874288d7043823@alex.mongohq.com:10020/app1717097?auto_reconnect=true'),
         querystring = require('querystring'),
         PORT = process.env.PORT || 8090,
         BASE_DIR = ".";
